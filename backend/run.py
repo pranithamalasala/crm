5ba32b552863f2e4720363800bcf3515fe0fd525
pranitha <pranithamalasala@gmail.com>
2026-04-25 18:58:22 +0530

    fix syntax error in run.py

--- backend/run.py
+++ backend/run.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     print("PORT USED:", port)
-    app.run(host="0.0.0.0", port=port)5000)
+    app.run(host="0.0.0.0", port=port)
